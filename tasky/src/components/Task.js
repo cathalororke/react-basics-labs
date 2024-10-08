25b272a43df1c5482c064d8ac1eefd2332813b86
Cathal,O Rorke 20070424 <20070424@wit.ie>
2024-10-08 21:36:38 +0100

    Completed Tasky Lab 1

---
+++ tasky/src/components/Task.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Task = (props) => {
+    
+    return (
+        <div className="card">
+            <p className="title">{props.title}</p>
+            <p>Due: {props.deadline}</p>
+            <p>{props.children}</p>
+            <p className="description">{props.description}</p>
+        </div>
+        
+    )
+}
+
+export default Task;
