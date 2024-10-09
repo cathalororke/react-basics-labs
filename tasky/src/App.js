7cd2b2da151fd52ed6ba6d4d862e0a4882792976
Cathal,O Rorke 20070424 <20070424@wit.ie>
2024-10-09 12:53:02 +0100

    Added keys to tasks

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -5,9 +5,9 @@ import Task from './components/Task';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { title: "Tidy up", description: "Clean Floors & Windows", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" }
     ]
   });
 
@@ -19,8 +19,9 @@ function App() {
       title={task.title}
       description={task.description}
       deadline={task.deadline}
+      key={task.id}
     />
-  ))} 
+  ))}
     </div>
   );
 }
