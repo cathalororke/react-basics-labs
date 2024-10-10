c520d31055eb56dad34da3d0d91d3a95182d4dba
Cathal,O Rorke 20070424 <20070424@wit.ie>
2024-10-10 14:08:59 +0100

    Mark task as done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -5,22 +5,30 @@ import Task from './components/Task';
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false}
     ]
   });
-
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+    console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+  }
     return (
     <div className="container">
       <h1>Tasky</h1>
-      {taskState.tasks.map((task) => (              
+      {taskState.tasks.map((task, index) => (              
     <Task 
       title={task.title}
       description={task.description}
       deadline={task.deadline}
       key={task.id}
+      done={task.done}
+      markDone={() => doneHandler(index)}
     />
+    
   ))}
     </div>
   );
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -1,14 +1,14 @@
-
 import React, { useState } from 'react';
 
 const Task = (props) => {
     
     return (
-        <div className="card">
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.children}</p>
             <p className="description">{props.description}</p>
+            <button onClick={props.markDone} className='doneButton'>Done</button>
         </div>
         
     )
